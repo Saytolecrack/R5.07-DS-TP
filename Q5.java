f32ba53e618d478608b8ebc2dabcdcddfb812c7a
Baltide Sacha <130437497+Saytolecrack@users.noreply.github.com>
2024-11-20 15:01:10 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,11 @@
+public class Q5 {
+    public static void main(String[] args) {
+        int n = 5; // Nombre de lignes
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("x"); // Remplacer '*' par 'x'
+            }
+            System.out.println();
+        }
+    }
+}
