d8ff3bde17103e617d6ef7086af190a308ce64c0
Baltide Sacha <130437497+Saytolecrack@users.noreply.github.com>
2024-11-20 14:38:31 +0100

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -1,1 +1,12 @@
-
+public class Q4 {
+  public static void main(String[] args) {
+    
+    int numberOfStars = int (args[0]);
+    String ligne = "*";
+      for (int i = 0; i < numberOfStars; i++) {
+          System.out.println(ligne);
+      ligne += "*";
+      }
+      System.out.println(); // Passe à la ligne après avoir affiché les étoiles
+  }
+}
